04ddccdadb8b0ba7f3b9bfb2c0fff0cbb25bb8c1
andresAmcs <andresamcs04@gmail.com>
2022-07-18 23:26:04 -0500

    a

--- src/componets/protected.jsx
+++ src/componets/protected.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 function Protected ({isLogin, children}){
     if (!isLogin) {
-        return <Navigate to="/login" replace/>
+        return <Navigate to="/Login" replace/>
                     
     }
     return children
